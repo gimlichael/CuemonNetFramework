0491e63ad08ae10de36af2a315a274be9e0eef68
gimlichael <mmdevelopment@hotmail.com>
2015-05-30 22:35:42 +0200

    Provided op-in for Factory creation of Dependency classes.

--- Cuemon/Dependency.cs
+++ Cuemon/Dependency.cs
@@ -44,6 +44,96 @@ namespace Cuemon
 
         #region Methods
         /// <summary>
+        /// Creates an instance of <typeparamref name="TResult" /> that implements the <see cref="IDependency"/> interface using a constructor of five parameters.
+        /// </summary>
+        /// <typeparam name="TResult">The type to create that implements the <see cref="IDependency"/> interface.</typeparam>
+        /// <returns>A reference to the newly created object.</returns>
+        public static TResult Create<TResult>() where TResult : IDependency
+        {
+            return ActivatorUtility.CreateInstance<TResult>();
+        }
+
+        /// <summary>
+        /// Creates an instance of <typeparamref name="TResult" /> that implements the <see cref="IDependency"/> interface using a constructor of five parameters.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter of the constructor.</typeparam>
+        /// <typeparam name="TResult">The type to create that implements the <see cref="IDependency"/> interface.</typeparam>
+        /// <param name="arg">The parameter of the constructor.</param>
+        /// <returns>A reference to the newly created object.</returns>
+        public static TResult Create<T, TResult>(T arg) where TResult : IDependency
+        {
+            return ActivatorUtility.CreateInstance<T, TResult>(arg);
+        }
+
+        /// <summary>
+        /// Creates an instance of <typeparamref name="TResult" /> that implements the <see cref="IDependency"/> interface using a constructor of five parameters.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the constructor.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the constructor.</typeparam>
+        /// <typeparam name="TResult">The type to create that implements the <see cref="IDependency"/> interface.</typeparam>
+        /// <param name="arg1">The first parameter of the constructor.</param>
+        /// <param name="arg2">The second parameter of the constructor.</param>
+        /// <returns>A reference to the newly created object.</returns>
+        public static TResult Create<T1, T2, TResult>(T1 arg1, T2 arg2) where TResult : IDependency
+        {
+            return ActivatorUtility.CreateInstance<T1, T2, TResult>(arg1, arg2);
+        }
+
+        /// <summary>
+        /// Creates an instance of <typeparamref name="TResult" /> that implements the <see cref="IDependency"/> interface using a constructor of five parameters.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the constructor.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the constructor.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter of the constructor.</typeparam>
+        /// <typeparam name="TResult">The type to create that implements the <see cref="IDependency"/> interface.</typeparam>
+        /// <param name="arg1">The first parameter of the constructor.</param>
+        /// <param name="arg2">The second parameter of the constructor.</param>
+        /// <param name="arg3">The third parameter of the constructor.</param>
+        /// <returns>A reference to the newly created object.</returns>
+        public static TResult Create<T1, T2, T3, TResult>(T1 arg1, T2 arg2, T3 arg3) where TResult : IDependency
+        {
+            return ActivatorUtility.CreateInstance<T1, T2, T3, TResult>(arg1, arg2, arg3);
+        }
+
+        /// <summary>
+        /// Creates an instance of <typeparamref name="TResult" /> that implements the <see cref="IDependency"/> interface using a constructor of five parameters.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the constructor.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the constructor.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter of the constructor.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter of the constructor.</typeparam>
+        /// <typeparam name="TResult">The type to create that implements the <see cref="IDependency"/> interface.</typeparam>
+        /// <param name="arg1">The first parameter of the constructor.</param>
+        /// <param name="arg2">The second parameter of the constructor.</param>
+        /// <param name="arg3">The third parameter of the constructor.</param>
+        /// <param name="arg4">The fourth parameter of the constructor.</param>
+        /// <returns>A reference to the newly created object.</returns>
+        public static TResult Create<T1, T2, T3, T4, TResult>(T1 arg1, T2 arg2, T3 arg3, T4 arg4) where TResult : IDependency
+        {
+            return ActivatorUtility.CreateInstance<T1, T2, T3, T4, TResult>(arg1, arg2, arg3, arg4);
+        }
+
+        /// <summary>
+        /// Creates an instance of <typeparamref name="TResult" /> that implements the <see cref="IDependency"/> interface using a constructor of five parameters.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter of the constructor.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter of the constructor.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter of the constructor.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter of the constructor.</typeparam>
+        /// <typeparam name="T5">The type of the fifth parameter of the constructor.</typeparam>
+        /// <typeparam name="TResult">The type to create that implements the <see cref="IDependency"/> interface.</typeparam>
+        /// <param name="arg1">The first parameter of the constructor.</param>
+        /// <param name="arg2">The second parameter of the constructor.</param>
+        /// <param name="arg3">The third parameter of the constructor.</param>
+        /// <param name="arg4">The fourth parameter of the constructor.</param>
+        /// <param name="arg5">The fifth parameter of the constructor.</param>
+        /// <returns>A reference to the newly created object.</returns>
+        public static TResult Create<T1, T2, T3, T4, T5, TResult>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) where TResult : IDependency
+        {
+            return ActivatorUtility.CreateInstance<T1, T2, T3, T4, T5, TResult>(arg1, arg2, arg3, arg4, arg5);
+        }
+
+        /// <summary>
         /// Marks the time when a dependency last changed.
         /// </summary>
         /// <param name="utcLastModified">The time when the dependency last changed.</param>
