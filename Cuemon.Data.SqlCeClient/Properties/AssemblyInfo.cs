cacd3f034dcba01851ba84ee7023ee887392825c
gimlichael <mmdevelopment@hotmail.com>
2015-06-01 02:07:12 +0200

    For reason unknown, this file was missing.

---
+++ Cuemon.Data.SqlCeClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+[assembly: AssemblyTitle(".NET Framework Additions")]
+[assembly: AssemblyDescription("This assembly contains support for the SQL Server Compact Edition 4.0.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Weubphoria")]
+[assembly: AssemblyProduct("Cuemon .NET Framework")]
+[assembly: AssemblyCopyright("Copyright © Weubphoria 2009-2015. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: EnvironmentPermissionAttribute(SecurityAction.RequestMinimum, Unrestricted = true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("63a8a7b0-68d7-4186-a4eb-42bb8f923805")]
+[assembly: AssemblyFileVersion("3.0.2015.1500")]
+[assembly: AssemblyVersion("3.0.2015.150")]
