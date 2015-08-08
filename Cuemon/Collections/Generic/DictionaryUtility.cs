96ba0135a4d1c6fbd3161707fc7f8b34770ad3f7
gimlichael <mmdevelopment@hotmail.com>
2015-08-08 23:49:41 +0200

    Added support for ToDictionary from its cleanest impl.; IEnumerable<KeyValuePair<TKey, TValue>>.

--- Cuemon/Collections/Generic/DictionaryUtility.cs
+++ Cuemon/Collections/Generic/DictionaryUtility.cs
@@ -43,5 +43,50 @@ namespace Cuemon.Collections.Generic
 			}
             return defaultValue;
 		}
-	}
+
+        /// <summary>
+        /// Creates a <see cref="Dictionary{TKey,TValue}"/> from the specified <paramref name="source"/> sequence.
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of values in the <paramref name="source"/>.</typeparam>
+        /// <param name="source">The sequence to create a <see cref="Dictionary{TKey,TValue}"/> from.</param>
+        /// <returns>A <see cref="Dictionary{TKey,TValue}"/> that is equivalent to the specified <paramref name="source"/> sequence.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="source"/> contains at least one <see cref="KeyValuePair{TKey,TValue}"/> that produces duplicate keys for two elements.
+        /// </exception>
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(IEnumerable<KeyValuePair<TKey, TValue>> source)
+        {
+            return ToDictionary(source, EqualityComparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Dictionary{TKey,TValue}"/> from the specified <paramref name="source"/> sequence.
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of values in the <paramref name="source"/>.</typeparam>
+        /// <param name="source">The sequence to create a <see cref="Dictionary{TKey,TValue}"/> from.</param>
+        /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing keys.</param>
+        /// <returns>A <see cref="Dictionary{TKey,TValue}"/> that is equivalent to the specified <paramref name="source"/> sequence.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> is null - or - <paramref name="comparer"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="source"/> contains at least one <see cref="KeyValuePair{TKey,TValue}"/> that produces duplicate keys for two elements.
+        /// </exception>
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(IEnumerable<KeyValuePair<TKey, TValue>> source, IEqualityComparer<TKey> comparer)
+        {
+            Validator.ThrowIfNull(source, "source");
+            Validator.ThrowIfNull(comparer, "comparer");
+
+            Dictionary<TKey, TValue> result = new Dictionary<TKey, TValue>(comparer);
+            foreach (KeyValuePair<TKey, TValue> item in source)
+            {
+                result.Add(item.Key, item.Value);
+            }
+            return result;
+        }
+    }
 }
