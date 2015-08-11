ca5ed4e0df75f6e1767d54a6953443a7e2457073
gimlichael <mmdevelopment@hotmail.com>
2015-08-11 06:03:17 +0200

    Exception details in documentation.

--- Cuemon/DateTimeUtility.cs
+++ Cuemon/DateTimeUtility.cs
@@ -91,6 +91,9 @@ namespace Cuemon
         /// <param name="interval">The <see cref="double"/> value that in combination with <paramref name="timeUnit"/> specifies the rounding of <paramref name="value"/>.</param>
         /// <param name="timeUnit">One of the enumeration values that specifies the time unit of <paramref name="interval"/>.</param>
         /// <returns>A <see cref="DateTime"/> value that is rounded towards negative infinity.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="interval"/> is 0.
+        /// </exception>
         public static DateTime Floor(DateTime value, double interval, TimeUnit timeUnit)
         {
             return Round(value, interval, timeUnit, VerticalDirection.Down);
@@ -103,6 +106,9 @@ namespace Cuemon
         /// <param name="interval">The <see cref="double"/> value that in combination with <paramref name="timeUnit"/> specifies the rounding of <paramref name="value"/>.</param>
         /// <param name="timeUnit">One of the enumeration values that specifies the time unit of <paramref name="interval"/>.</param>
         /// <returns>A <see cref="DateTime"/> value that is rounded towards positive infinity.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="interval"/> is 0.
+        /// </exception>
         public static DateTime Ceiling(DateTime value, double interval, TimeUnit timeUnit)
         {
             return Round(value, interval, timeUnit, VerticalDirection.Up);
@@ -118,6 +124,9 @@ namespace Cuemon
         /// <returns>A <see cref="DateTime"/> value that is rounded either towards negative infinity or positive infinity.</returns>
         /// <exception cref="InvalidEnumArgumentException">
         /// <paramref name="direction"/> is an invalid enumeration value.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="interval"/> is <see cref="TimeSpan.Zero"/>.
         /// </exception>
         public static DateTime Round(DateTime value, double interval, TimeUnit timeUnit, VerticalDirection direction)
         {
