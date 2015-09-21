07008887b2c925b9b4007aec376ada77c27bb4c5
gimlichael <mmdevelopment@hotmail.com>
2015-09-21 23:58:44 +0200

    Refactoring spawned this new utility class.

---
+++ Cuemon.Web/HttpRuntimeUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using Cuemon.Reflection;
+
+namespace Cuemon.Web
+{
+    /// <summary>
+    /// This utility class is designed to make <see cref="HttpRuntime"/> operations easier to work with.
+    /// </summary>
+    public static class HttpRuntimeUtility
+    {
+        private static bool _supportsIisIntegratedPipelineMode = InitSupportsIisIntegratedPipelineMode();
+
+        private static bool InitSupportsIisIntegratedPipelineMode()
+        {
+            try
+            {
+                return HttpRuntime.UsingIntegratedPipeline;
+            }
+            catch (MissingMethodException)
+            {
+                Type httpRuntimeType = typeof(HttpRuntime);
+                FieldInfo useIntegratedPipeline = ReflectionUtility.GetField(httpRuntimeType, "_useIntegratedPipeline", ReflectionUtility.BindingInstancePublicAndPrivateNoneInheritedIncludeStatic);
+                if (useIntegratedPipeline != null &&
+                    useIntegratedPipeline.FieldType == typeof(bool))
+                {
+                    return (bool)useIntegratedPipeline.GetValue(null);
+                }
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the executing platform of this ASP.NET implementation supports IIS integrated pipeline mode.
+        /// </summary>
+        /// <value><c>true</c> if the executing platform of this ASP.NET implementation supports IIS integrated pipeline mode; otherwise, <c>false</c>.</value>
+        public static bool SupportsIisIntegratedPipelineMode
+        {
+            get { return _supportsIisIntegratedPipelineMode; }
+            internal set { _supportsIisIntegratedPipelineMode = value; }
+
+        }
+    }
+}
