5f7d9b7e0b4214b72b085e0b9e5c7eea48ff7582
gimlichael <mmdevelopment@hotmail.com>
2015-09-21 19:48:56 +0200

    New interface for Time Measuring.

---
+++ Cuemon/Diagnostics/ITimeMeasuring.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Cuemon.Diagnostics
+{
+    /// <summary>
+    /// Specifies a simple way to measure elapsed time for events or similar.
+    /// </summary>
+    public interface ITimeMeasuring
+    {
+        /// <summary>
+        /// Gets the timer that is used to accurately measure elapsed time.
+        /// </summary>
+        /// <value>The timer that is used to accurately measure elapsed time.</value>
+        Stopwatch Timer { get; }
+
+        /// <summary>
+        /// Gets or sets the callback delegate for the measured elapsed time.
+        /// </summary>
+        /// <value>The callback delegate for the measured elapsed time.</value>
+        Act<string, TimeSpan> TimeMeasuringCallback { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether time measuring is enabled.
+        /// </summary>
+        /// <value><c>true</c> if time measuring is enabled; otherwise, <c>false</c>.</value>
+        bool EnableTimeMeasuring { get; set; }
+    }
+}
