0870a0d48ee769364e382a865581ec3fb8cb6ee1
gimlichael <mmdevelopment@hotmail.com>
2016-12-18 15:31:13 +0100

    MaximumAllowedLatency

--- Cuemon/TransientOperationOptions.cs
+++ Cuemon/TransientOperationOptions.cs
@@ -11,12 +11,42 @@ namespace Cuemon
         /// <summary>
         /// Initializes a new instance of the <see cref="TransientOperationOptions"/> class.
         /// </summary>
+        /// <remarks>
+        /// The following table shows the initial property values for an instance of <see cref="TransientOperationOptions"/>.
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Property</term>
+        ///         <description>Initial Value</description>
+        ///     </listheader>
+        ///     <item>
+        ///         <term><see cref="RetryAttempts"/></term>
+        ///         <description><see cref="DefaultRetryAttempts"/></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="EnableRecovery"/></term>
+        ///         <description><see cref="RetryAttempts"/> > 0</description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="RetryStrategy"/></term>
+        ///         <description><c>retry</c> + 2^ to a maximum of 5; eg. 1, 2, 4, 8, 16 to a total of 31 seconds</description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="DetectionStrategy"/></term>
+        ///         <description><c>exception => false</c></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="MaximumAllowedLatency"/></term>
+        ///         <description><c>5 minutes</c></description>
+        ///     </item>
+        /// </list>
+        /// </remarks>
         public TransientOperationOptions()
         {
             RetryAttempts = DefaultRetryAttempts;
             EnableRecovery = RetryAttempts > 0;
             RetryStrategy = currentAttempt => TimeSpan.FromSeconds(Math.Pow(2, currentAttempt > 5 ? 5 : currentAttempt));
             DetectionStrategy = exception => false;
+            MaximumAllowedLatency = TimeSpan.FromMinutes(5);
         }
 
         /// <summary>
@@ -36,7 +66,7 @@ namespace Cuemon
         /// Gets or sets the callback function delegate that determines the amount of time to wait for a transient fault to recover gracefully before trying a new attempt.
         /// </summary>
         /// <returns>A <see cref="Doer{TResult}"/> that determines the amount of time to wait for a transient fault to recover gracefully.</returns>
-        /// <remarks>Default implementation is <see cref="int"/> + 2^ to a maximum of 5; eg. 1, 2, 4, 8, 16 to a total of 32 seconds.</remarks>
+        /// <remarks>Default implementation is <see cref="int"/> + 2^ to a maximum of 5; eg. 1, 2, 4, 8, 16 to a total of 31 seconds.</remarks>
         public Doer<int, TimeSpan> RetryStrategy { get; set; }
 
         /// <summary>
@@ -45,6 +75,12 @@ namespace Cuemon
         /// <value>A <see cref="Doer{TResult}"/> that determines if an <see cref="Exception"/> contains clues that would suggest a transient fault.</value>
         /// <remarks>Default implementation is fixed to none-transient failure.</remarks>
         public Doer<Exception, bool> DetectionStrategy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum allowed latency before a <see cref="LatencyException"/> is raised.
+        /// </summary>
+        /// <value>A <see cref="TimeSpan"/> defining the maximum allowed latency.</value>
+        public TimeSpan MaximumAllowedLatency { get; set; }
 
         /// <summary>
         /// Gets or sets the default amount of retry attempts for transient faults. Default is 5 attempts.
